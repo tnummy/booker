0b07e8234c785fb67747c8dfa3790ea8917b775d
Tim <tnummy@users.noreply.github.com>
2018-11-11 01:35:02 -0500

    Update nav.blade.php
    Remove user id from navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -37,7 +37,7 @@
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                            {{ Auth::user()->first_name }} {{Auth::user()->id}} <span class="caret"></span>
+                            {{ Auth::user()->first_name }} <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu">
